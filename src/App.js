b3b45c50eddfa505672103d1773a5a2bb9968429
jbeyer15 <masterchef325@yahoo.com>
2024-03-12 08:59:27 -0600

    intial draft

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Cry from "./cry";
+
+function App() {
+
+  let [pokemonIndex, setPokemonIndex] = useState(1);
+  let [selectedPokemon, setSelectedPokemon] = useState({ name: "Missing No." });
+  let [pokemonDescription, setPokemonDescription] = useState({ description: "It's MissingNo!" });
+  let [url, setUrl] = useState("https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/1.ogg");
+
+  const increaseClick = () => {
+    let index = pokemonIndex + 1;
+    setPokemonIndex(index);
+  }
+
+  const decreaseClick = () => {
+    let index = pokemonIndex - 1;
+    setPokemonIndex(index);
+  }
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`)
+      .then(response => response.json())
+      .then(pokemonData => {
+        setSelectedPokemon(pokemonData);
+      })
+  }, [pokemonIndex])
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}`)
+      .then(response => response.json())
+      .then(description => {
+        setPokemonDescription(description);
+      })
+  }, [pokemonIndex])
+
+  useEffect(() => {
+    setUrl(`https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokemonIndex}.ogg`)
+
+  }, [pokemonIndex])
+
+  if(selectedPokemon.name == "Missing No." && pokemonDescription.description == "It's MissingNo!"){
+    return(
+      <h1>LOADING</h1>
+    )
+  }
+
+
+
+  return (
+    <div className="App">
+      <h1>The Pokedex</h1>
+      <div>
+        <h2>NO. {pokemonIndex}</h2>
+        <h3>{selectedPokemon.name}</h3>
+      </div>
+      {
+        selectedPokemon.sprites == undefined ?
+          <img src={"https://miro.medium.com/v2/resize:fit:340/1*Oy3w5Sf30aI8cAObDDHRCA.jpeg"} /> :
+          <img width="300px" src={selectedPokemon.sprites.front_default} />
+      }
+      {
+        pokemonDescription.flavor_text_entries[0].flavor_text == undefined ?
+          <p>THIS POKEMON IS NOT DISCOVERED</p> :
+          <p>{pokemonDescription.flavor_text_entries[0].flavor_text}</p>
+      }
+      <div>
+        <button onClick={decreaseClick}>Previous Page</button>
+        <button onClick={increaseClick}>Next Page</button>
+      </div>
+
+      <Cry url={url} />
+
+
+
+    </div>
+  );
+}
+export default App;
---
+++ src/cry.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+const Cry = ({ url }) => {
+  console.log("TRYING WITH LINK: ", url)
+
+
+
+  return (
+    <div>
+
+
+
+    </div>
+  )
+
+
+}
+
+export default Cry;
